055ad8f34f9836acb1e5ea3f9a2f0781561c0f51
Rasujon3 <rasujon3@gmail.com>
2022-05-11 03:45:13 +0600

    Simple node mongo server for doctors portal

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -8,6 +9,8 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dprcj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 app.get("/", (req, res) => {
   res.send("Running `Doctors Portal` Server");
