3ebb4753b7db5a154631479f8517fd8352cf9bee
Rasujon3 <rasujon3@gmail.com>
2022-05-14 03:05:04 +0600

    74-6 bug solved

--- index.js
+++ index.js
@@ -42,26 +42,26 @@ async function run() {
     app.get("/available", async (req, res) => {
       const date = req.query.date;
 
-      // step 1: get all services
+      // step 1:  get all services
       const services = await serviceCollection.find().toArray();
 
-      // step 2: get the booking of that day. output: [{},{},{},{},{},{},{},{}]
+      // step 2: get the booking of that day. output: [{}, {}, {}, {}, {}, {}]
       const query = { date: date };
       const bookings = await bookingCollection.find(query).toArray();
 
       // step 3: for each service
       services.forEach((service) => {
-        // step 4: find bookings for that service. output: [{},{},{},{}]
+        // step 4: find bookings for that service. output: [{}, {}, {}, {}]
         const serviceBookings = bookings.filter(
           (book) => book.treatment === service.name
         );
-        // step 5: select slots for service Bookings: ["","","",""]
+        // step 5: select slots for the service Bookings: ['', '', '', '']
         const bookedSlots = serviceBookings.map((book) => book.slot);
         // step 6: select those slots that are not in bookedSlots
         const available = service.slots.filter(
           (slot) => !bookedSlots.includes(slot)
         );
-        // step 7: set available to slots to make it easier
+        //step 7: set available to slots to make it easier
         service.slots = available;
       });
 
