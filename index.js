559bc80f266c7d5ad7ff141cd56871b474184eb0
Rasujon3 <rasujon3@gmail.com>
2022-05-14 23:21:21 +0600

    75-1 User Creation Process

--- index.js
+++ index.js
@@ -27,12 +27,29 @@ async function run() {
     const bookingCollection = client
       .db(`doctors_portal`)
       .collection(`bookings`);
+    const userCollection = client.db(`doctors_portal`).collection(`users`);
 
     app.get("/service", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
+    });
+
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: user,
+      };
+      const result = await productsCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
+      res.send(result);
     });
 
     // warning
@@ -96,6 +113,7 @@ async function run() {
      * app.get('/booking') // get a specific booking
      * app.post('/booking') // add a new booking
      * app.patch('/booking/:id') //
+     * app.put('/booking/:id') // upsert => update (if exists) or insert (id doesn't exist)
      * app.delete('/booking/:id') //
      */
   } finally {
