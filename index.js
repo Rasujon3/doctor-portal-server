2ecbef741ea8c29cf1edfe66adaf3a2ac2dd4b2b
Rasujon3 <rasujon3@gmail.com>
2022-05-17 02:43:50 +0600

    add a doctor post method api

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ async function run() {
       .db(`doctors_portal`)
       .collection(`bookings`);
     const userCollection = client.db(`doctors_portal`).collection(`users`);
+    const doctorCollection = client.db(`doctors_portal`).collection(`doctors`);
 
     app.get("/service", async (req, res) => {
       const query = {};
@@ -164,6 +165,14 @@ async function run() {
       }
       const result = await bookingCollection.insertOne(booking);
       return res.send({ success: true, result });
+    });
+
+    // doctor API
+
+    app.post("/doctor", async (req, res) => {
+      const doctor = req.body;
+      const result = await doctorCollection.insertOne(doctor);
+      res.send(result);
     });
 
     /*
